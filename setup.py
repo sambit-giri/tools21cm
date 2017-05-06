bd39f68b2caa81eab722b534bf978c48098bf53b
Sambit Kumar Giri <sgiri@astro.su.se>
2017-05-06 13:21:57 +0200

    Restructured

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ Created on 12 April 2017
 Setup script
 '''
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(name='py21cmtools',
       version='0.1',
       author='Sambit Giri',
       author_email='sambit.giri@astro.su.se',
-      package_dir = {'py21cmtools' : 'source'},
+      package_dir = {'py21cmtools' : 'src'},
       packages=['py21cmtools'],
-      )
+      include_package_data=True,
+)
