2c897d7bb51e21d882880edecbb7e933525d3955
Sambit Kumar Giri <sgiri@astro.su.se>
2017-05-06 12:28:51 +0200

    lyman alpha coupling

--- source/lyman_alpha_coupling.py
+++ source/lyman_alpha_coupling.py
@@ -3,7 +3,7 @@ import c2raytools as c2t
 from scipy.interpolate import interp1d
 from usefuls import *
 import os
-import time
+import time, timeit
 
 lam_lya   = 1215.                #Ang
 lam_HII   = 912.                 #Ang
@@ -12,6 +12,7 @@ dlam      = 1                    #Ang
 h_planck  = 6.626e-34            #m^2 kg / s
 c_light   = 3e8                  #m/s
 nu_lya    = c_light/1215.67e-10  #Hz
+E_lya     = 10.19884             #eV
 
 def _read_halofile(filename, z=-1):
 	if filename.split('-')[-1] == 'coarsest_sources_used_wfgamma.dat': src = np.loadtxt(filename, skiprows=1)
@@ -39,6 +40,7 @@ def Lya_coupling_coeff(ncells, boxsize, sourcelist, z=-1, SED=None, lum=None, la
 	
 
 def one_source(z, ncells, boxsize, source_pos, mass, sed_func):
+	start = timeit.default_timer()
 	i,j,k    = source_pos
 	xx,yy,zz = np.mgrid[0:ncells,0:ncells,0:ncells]
 	rr2 = ((xx-i)**2 + (yy-j)**2 + (zz-k)**2)*boxsize**2/ncells**2
@@ -49,6 +51,8 @@ def one_source(z, ncells, boxsize, source_pos, mass, sed_func):
 	eng = sed_func(lms)*mass*dlam/(4*np.pi*rp2)
 	eng[lms<=lam_HII] = 0.
 	n_a = eng/h_planck/nu_lya
+	stop  = timeit.default_timer()
+	print stop-start
 	return n_a
 
 def Lya_coupling_coeff_MPI(ncells, boxsize, sourcelist_loc, z=-1, SED_loc=None, n_jobs=5, lum=None, lam=None):
@@ -67,6 +71,46 @@ def Lya_coupling_coeff_MPI(ncells, boxsize, sourcelist_loc, z=-1, SED_loc=None, 
 	xc_cube = np.load(flname)
 	os.remove(flname)
 	return xc_cube
+
+def Lya_coupling_coeff_(ncells, boxsize, sourcelist, z=-1, SED=None, lum=None, lam=None):
+	assert SED is not None or (lum is not None and lam is not None)
+	if SED is not None and type(SED) == str: lum, lam = np.loadtxt(SED)
+	elif SED is not None: lum, lam = SED
+	if (lum is not None and lam is not None):
+		if type(lum)==str: lum = np.loadtxt(lum)   # Energy/time/mass
+		if type(lam)==str: lam = np.loadtxt(lam)   # angstrom
+	sed_func = interp1d(lam, lum, kind='cubic')
+	rr  = 10**np.linspace(-2,np.log10(boxsize*1.8),1000)
+	zs  = c2t.cdist_to_z(rr+c2t.z_to_cdist(z))
+	zs_HII = zs[lms<=lam_HII].min()
+	lms = lam_lya*(1+z)/(1+zs)
+	rp  = rr/(1.0+zs)
+	eng = sed_func(lms)*dlam/(4*np.pi*rp**2)
+	eng_func = interp1d(zs, eng, kind='cubic')
+	src, z   = _read_halofile(sourcelist, z=z)
+	n_src    = src.shape[0]
+	masses   = Mgrid_2_Msolar(src[:,3])               # in solar mass
+	xc_cube  = np.zeros((ncells, ncells, ncells))
+	xx,yy,zz = np.mgrid[0:ncells,0:ncells,0:ncells]
+	for s in xrange(n_src):
+		source_pos = src[s,:-1]-np.ones(3)
+		na_cube  = one_source(xx, yy, zz, ncells, boxsize, source_pos, masses[p], eng_func, zs_HII)
+		xc_cube += na_cube
+		loading_verbose(str((s+1)*100/n_src)+'%')
+	return xc_cube
+
+def one_source_(xx, yy, zz, z, ncells, boxsize, source_pos, mass, eng_func, zs_HII):
+	start = timeit.default_timer()
+	i,j,k = source_pos
+	rr2 = ((xx-i)**2 + (yy-j)**2 + (zz-k)**2)*boxsize**2/ncells**2
+	rr  = np.sqrt(rr2); rr[rr==0] = 0.1
+	zss = c2t.cdist_to_z(rr+c2t.z_to_cdist(z))
+	eng = eng_func(zss)
+	eng[zss>=zs_HII] = 0.
+	n_a = eng*mass/E_lya
+	stop  = timeit.default_timer()
+	print stop-start
+	return n_a
 
 
 
