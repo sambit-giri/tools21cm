06882b1a6865f2bd6b0de646f432a1b080b20ecd
Sambit Kumar Giri <sgiri@astro.su.se>
2017-05-09 11:58:53 +0200

    contribution from previous sources added

--- src/lyman_alpha_coupling.py
+++ src/lyman_alpha_coupling.py
@@ -2,7 +2,7 @@ import numpy as np
 import c2raytools as c2t
 from scipy.interpolate import interp1d, interp2d
 from usefuls import *
-import os
+import os, glob
 import time, timeit
 
 lam_lya   = 1215.                #Ang
@@ -13,6 +13,8 @@ h_planck  = 6.626e-34            #m^2 kg / s
 c_light   = 3e8                  #m/s
 nu_lya    = c_light/1215.67e-10  #Hz
 E_lya     = 10.19884             #eV
+m2parsec  = 3.24e-17
+yr2sec    = 3.154e7
 
 def _read_halofile(filename, z=-1):
 	if filename.split('-')[-1] == 'coarsest_sources_used_wfgamma.dat': src = np.loadtxt(filename, skiprows=1)
@@ -112,20 +114,52 @@ def one_source_(xx, yy, zz, z, ncells, boxsize, source_pos, mass, eng_func, zs_H
 	print stop-start
 	return n_a
 
-def Lya_coupling_coeff_useprofile(ncells, boxsize, sourcelist, z=-1, SED=None, lum=None, lam=None):
+def Lya_coupling_coeff_useprofile(z, ncells, boxsize, source_dir, source_file='*-coarsest_sources_used_wfgamma.dat', SED=None, lum=None, lam=None, tstep=11.5):
 	assert SED is not None or (lum is not None and lam is not None)
-	if SED is not None and type(SED) == str: lum, lam = np.loadtxt(SED)
+	if SED is not None and type(SED) == str: lam, lum = np.loadtxt(SED).T
 	elif SED is not None: lum, lam = SED
 	if (lum is not None and lam is not None):
 		if type(lum)==str: lum = np.loadtxt(lum)   # Energy/time/mass
 		if type(lam)==str: lam = np.loadtxt(lam)   # angstrom
+	sourcelists = np.array(glob.glob(source_dir+source_file))
+	source_zs   = np.array([float(ss.split('/')[-1].split('-')[0]) for ss in sourcelists])
+	if not(z in source_zs): 
+		z = source_zs[np.abs(source_zs-z).argmin()]
+		print "The nearest sourcelist in the given directory is of z =", z
+	z_max   = z+(lam_lya/lam_HII - 1.)
+	src_zs  = source_zs[source_zs>=z]
+	src_zs  = src_zs[src_zs<=z_max]
+	r_light = c_light*tstep*yr2sec*m2parsec
+	for zi in src_zs:
+		if zi==z: zl = -1
+		else: zl = source_zs[source_zs<zi].max()
+		engs    = _get_profile(z, lam, lum, boxsize, ncells, r_light*(1+zl), r_light*(1+zi))
+		sources = np.loadtxt(sourcelists[source_zs==zi][0], skiprows=1)
+		xc_cube = _get_xc(sources, engs, ncells)
+	return xc_cube
+
+def _get_xc(sources, engs, ncells, mass_axis=3, time_taken=False):
+	start = timeit.default_timer()
+	masses  = Mgrid_2_Msolar(sources[:,mass_axis])
+	xc_cube = np.zeros((ncells, ncells, ncells))
+	n_src   = sources.shape[0]
+	for s in xrange(n_src):
+		i,j,k = sources[s,:3]-np.ones(3)
+		mass  = masses[s]
+		eng_  = engs[(ncells-i):(2*ncells-i),(ncells-j):(2*ncells-j),(ncells-k):(2*ncells-k)]
+		xc_cube += eng_*mass/E_lya
+	stop  = timeit.default_timer()
+	if time_taken: print (stop-start), "seconds"
+	return xc_cube
+	
+
+def _get_profile(z, lam, lum, boxsize, ncells, r_min, r_max):
 	sed_func = interp1d(lam, lum, kind='cubic')
 	rs = 10**np.linspace(-2,np.log10(boxsize*1.8),1000)
-	#rr  = np.hstack((-rr_,rr_)); rr.sort()
 	zs  = c2t.cdist_to_z(rs+c2t.z_to_cdist(z))
 	lms = lam_lya*(1+z)/(1+zs)
-	rs_HII = rs[lms<=lam_HII].min()
 	rp  = rs/(1.0+zs)
+	rs_HII = rs[lms<=lam_HII].min()
 	eng = sed_func(lms)*dlam/(4*np.pi*rp**2)
 	xx,yy,zz = np.mgrid[-ncells:ncells,-ncells:ncells,-ncells:ncells]
 	rr2 = (xx**2 + yy**2 + zz**2)*boxsize**2/ncells**2
@@ -133,14 +167,14 @@ def Lya_coupling_coeff_useprofile(ncells, boxsize, sourcelist, z=-1, SED=None, l
 	eng_func = interp1d(rs, eng, kind='cubic')
 	engs     = eng_func(rr)
 	engs[rr>=rs_HII] = 0.
-	masses   = Mgrid_2_Msolar(src[:,3])
-	xc_cube  = np.zeros((ncells, ncells, ncells))
-	for s in xrange(n_src):
-		i,j,k = src[s,:-1]-np.ones(3)
-		mass  = masses[s]
-		eng_  = engs[(ncells-i):(2*ncells-i),(ncells-j):(2*ncells-j),(ncells-k):(2*ncells-k)]
-		xc_cube += eng_*mass/E_lya
-	return xc_cube
+	engs[rr<=r_min]  = 0.
+	engs[rr>=r_max]  = 0.
+	eng0 = np.roll(engs, ncells, axis=0)
+	eng1 = np.roll(engs, ncells, axis=1)
+	eng2 = np.roll(engs, ncells, axis=2)
+	engs = engs+eng0+eng1+eng2
+	return engs
+
 
 	
 	
