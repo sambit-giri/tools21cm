0e9aa50f80425c89ab3d31eb83ebc7960b1093d5
Sambit Kumar Giri <sgiri@astro.su.se>
2017-10-20 10:20:02 +0200

    read files from dir

--- src/read_files.py
+++ src/read_files.py
@@ -65,4 +65,63 @@ def read_grizzly_files(filename):
 	"""
 	return c2t.XfracFile(filename).xi
 
+
+def coeval_21cm_c2ray(xfrac_dir, dens_dir, z, interpolation='linear', mean_subtract=False):
+	"""
+	xfrac_dir     : Give the path that contains the xfrac-files.
+	dens_dir      : Give the path that contains the density-files.
+	z	      : Redshift.
+	interpolation : This is used when the coveal cube at that redshift is not available.
+	"""
+	xfrac = coeval_xfrac(xfrac_dir, z, interpolation=interpolation)
+	dens  = coeval_dens(dens_dir, z, interpolation=interpolation)
+	dt    = c2t.calc_dt(xfrac, dens, z=z)
+	if mean_subtract: return dt-dt.mean()
+	else: return dt
+
+
+def coeval_xfrac_c2ray(xfrac_dir, z, interpolation='linear'):
+	"""
+	xfrac_dir     : Give the path that contains the xfrac-files.
+	z	      : Redshift.
+	interpolation : This is used when the coveal cube at that redshift is not available.
+	"""
+	if not interpolation in ['linear']: #, 'step', 'sigmoid', 'step_cell'
+		raise ValueError('Unknown interpolation type: %s' % interpolation)
+	xfrac_files = glob.glob(xfrac_dir + '/xfrac3d_*.bin')
+	xfrac_zs = None
+	xfrac_zs = c2t.lightcone._get_file_redshifts(xfrac_zs, xfrac_files)
+	if z in xfrac_zs:
+		xfrac = c2t.XfracFile(xfrac_files[np.argwhere(z==xfrac_zs)]).xi
+	else:
+		z_l = xfrac_zs[xfrac_zs<z].max()
+		z_h = xfrac_zs[xfrac_zs>z].min()
+		xfrac_l = c2t.XfracFile(xfrac_files[xfrac_zs[xfrac_zs<z].argmax()]).xi
+		xfrac_h = c2t.XfracFile(xfrac_files[xfrac_zs[xfrac_zs>z].argmin()]).xi
+		xfrac = xfrac_h + (xfrac_l-xfrac_h)*(z-z_h)/(z_l-z_h)
+		print "The xfrac cube has been interpolated using", interpolation, "interpolation."
+		print "CAUTION: This data should be used with care."
+	return xfrac
+
+def coeval_dens_c2ray(dens_dir, z):
+	"""
+	dens_dir      : Give the path that contains the density-files.
+	z	      : Redshift.
+	interpolation : This is used when the coveal cube at that redshift is not available.
+	"""
+	dens_files  = glob.glob(dens_dir + '/*n_all.dat')
+	dens_zs  = None
+	dens_zs  = c2t.lightcone._get_file_redshifts(dens_zs, dens_files)
+	if z in dens_zs:
+		dens, dtype = c2t.helper_functions.get_data_and_type(dens_files[np.argwhere(z==dens_zs)])
+	else:
+		#z_l = dens_zs[dens_zs<z].max()
+		z_h = dens_zs[dens_zs>z].min()
+		#dens_l,dtype = c2t.helper_functions.get_data_and_type(dens_files[dens_zs[dens_zs<z].argmax()])
+		dens_h, dtype = c2t.helper_functions.get_data_and_type(dens_files[dens_zs[dens_zs>z].argmin()])
+		#dens = dens_h + (dens_l-dens_h)*(z-z_h)/(z_l-z_h)
+		dens = dens_h*(z/z_h)**3
+		#print "The density cube has been interpolated using", interpolation, "interpolation."
+		print "The density has been scaled from the density at the previous time."
+	return dens.astype(np.float64)
 	
