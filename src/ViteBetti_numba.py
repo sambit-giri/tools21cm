4c9ea6068d81e11f360f33af110eb93758d8a3da
sambit-giri <sambitgiri@outlook.com>
2018-09-09 20:41:21 +0200

    markers to the cubemap

--- src/ViteBetti_numba.py
+++ src/ViteBetti_numba.py
@@ -3,42 +3,35 @@ from scipy.ndimage import filters
 from numba import autojit, prange
 
 @autojit
-def CubeMap(arr):
+def CubeMap(arr, multi_marker=True):
 	nx, ny, nz = arr.shape
-	Nx, Ny, Nz = 2*nx-1,2*ny-1,2*nz-1
+	Nx, Ny, Nz = 2*nx-1,2*ny-1,2*nz-1#2*nx,2*ny,2*nz#
 	cubemap    = np.zeros((Nx,Ny,Nz))
+	markers    = 1, 1, 1, 1
+	if multi_marker: markers = 1, 2, 3, 4
 	## Vertices
 	for i in prange(nx):
 		for j in xrange(ny):
 			for k in xrange(nz):
-				if arr[i,j,k]: cubemap[i*2,j*2,k*2] = 1
+				if arr[i,j,k]: cubemap[i*2,j*2,k*2] = markers[0]
 
 	## Edges 
-	stele = np.zeros((3,3,3)); stele[:,1,1] = 1
-	dummy = filters.convolve(cubemap, stele, mode='constant')*(1-cubemap)
-	cubemap[dummy==2] = 1
-	stele = np.zeros((3,3,3)); stele[1,:,1] = 1
-	dummy = filters.convolve(cubemap, stele, mode='constant')
-	cubemap[dummy==2] = 1
-	stele = np.zeros((3,3,3)); stele[1,1,:] = 1
-	dummy = filters.convolve(cubemap, stele, mode='constant')
-	cubemap[dummy==2] = 1
 	for i in prange(Nx):
 		for j in xrange(Ny):
 			for k in xrange(Nz):
 				if cubemap[i,j,k] == 0:
-					if cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k]: cubemap[i,j,k] = 1
-					elif cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k]: cubemap[i,j,k] = 1
-					elif cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = 1
+					if cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k]: cubemap[i,j,k] = markers[1]
+					elif cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k]: cubemap[i,j,k] = markers[1]
+					elif cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = markers[1]
 
 	## Faces 
 	for i in prange(Nx):
 		for j in xrange(Ny):
 			for k in xrange(Nz):
 				if cubemap[i,j,k] == 0:
-					if cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k] and cubemap[i,(j-1)%Ny,k]==1 and cubemap[i,(j+1)%Ny,k]: cubemap[i,j,k] = 1
-					elif cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k] and cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = 1
-					elif cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz] and cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k]: cubemap[i,j,k] = 1
+					if cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k] and cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k]: cubemap[i,j,k] = markers[2]
+					elif cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k] and cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = markers[2]
+					elif cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz] and cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k]: cubemap[i,j,k] = markers[2]
 	
 	## Cubes
 	for i in prange(Nx):
@@ -47,7 +40,7 @@ def CubeMap(arr):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1)%Nx,j,k] and cubemap[(i+1)%Nx,j,k]: 
 						if cubemap[i,(j-1)%Ny,k] and cubemap[i,(j+1)%Ny,k]: 
-							if cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = 1
+							if cubemap[i,j,(k-1)%Nz] and cubemap[i,j,(k+1)%Nz]: cubemap[i,j,k] = markers[3]
 
 	return cubemap	
 
