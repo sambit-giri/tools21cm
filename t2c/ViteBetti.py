8af83d5dfb92178519629afb44e8fb04fa41863e
Sambit Giri <sambitgiri@Sambits-MacBook-Pro.local>
2020-08-05 13:02:58 +0200

    minor fix

--- t2c/ViteBetti.py
+++ t2c/ViteBetti.py
@@ -8,33 +8,33 @@ def CubeMap(arr, multi_marker=True):
 	markers    = 1, 1, 1, 1
 	if multi_marker: markers = 1, 2, 3, 4
 	## Vertices
-	for i in xrange(nx):
-		for j in xrange(ny):
-			for k in xrange(nz):
+	for i in range(nx):
+		for j in range(ny):
+			for k in range(nz):
 				if arr[i,j,k]: cubemap[i*2,j*2,k*2] = markers[0]
 
 	## Edges 
-	for i in xrange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+	for i in range(Nx):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[0] and cubemap[(i+1)%Nx,j,k]==markers[0]: cubemap[i,j,k] = markers[1]
 					elif cubemap[i,(j-1),k]==markers[0] and cubemap[i,(j+1)%Ny,k]==markers[0]: cubemap[i,j,k] = markers[1]
 					elif cubemap[i,j,(k-1)]==markers[0] and cubemap[i,j,(k+1)%Nz]==markers[0]: cubemap[i,j,k] = markers[1]
 
 	## Faces 
-	for i in xrange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+	for i in range(Nx):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[1] and cubemap[(i+1)%Nx,j,k]==markers[1] and cubemap[i,(j-1),k]==markers[1] and cubemap[i,(j+1)%Ny,k]==markers[1]: cubemap[i,j,k] = markers[2]
 					elif cubemap[i,(j-1),k]==markers[1] and cubemap[i,(j+1)%Ny,k]==markers[1] and cubemap[i,j,(k-1)]==markers[1] and cubemap[i,j,(k+1)%Nz]==markers[1]: cubemap[i,j,k] = markers[2]
 					elif cubemap[i,j,(k-1)]==markers[1] and cubemap[i,j,(k+1)%Nz]==markers[1] and cubemap[(i-1),j,k]==markers[1] and cubemap[(i+1)%Nx,j,k]==markers[1]: cubemap[i,j,k] = markers[2]
 	
 	## Cubes
-	for i in xrange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+	for i in range(Nx):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[2] and cubemap[(i+1)%Nx,j,k]==markers[2]: 
 						if cubemap[i,(j-1),k]==markers[2] and cubemap[i,(j+1)%Ny,k]==markers[2]: 
@@ -46,9 +46,9 @@ def CubeMap(arr, multi_marker=True):
 def EulerCharacteristic_seq(A):
 	chi = 0;
 	nx,ny,nz = A.shape
-	for x in xrange(nx):
-		for y in xrange(ny):
-			for z in xrange(nz):
+	for x in range(nx):
+		for y in range(ny):
+			for z in range(nz):
 				if(A[x,y,z] == 1):
 					if (x+y+z)%2 == 0: chi += 1
 					else: chi -= 1
--- t2c/ViteBetti_numba.py
+++ t2c/ViteBetti_numba.py
@@ -11,14 +11,14 @@ def CubeMap(arr, multi_marker=True):
 	if multi_marker: markers = 1, 2, 3, 4
 	## Vertices
 	for i in prange(nx):
-		for j in xrange(ny):
-			for k in xrange(nz):
+		for j in range(ny):
+			for k in range(nz):
 				if arr[i,j,k]: cubemap[i*2,j*2,k*2] = markers[0]
 
 	## Edges 
 	for i in prange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[0] and cubemap[(i+1)%Nx,j,k]==markers[0]: cubemap[i,j,k] = markers[1]
 					elif cubemap[i,(j-1),k]==markers[0] and cubemap[i,(j+1)%Ny,k]==markers[0]: cubemap[i,j,k] = markers[1]
@@ -26,8 +26,8 @@ def CubeMap(arr, multi_marker=True):
 
 	## Faces 
 	for i in prange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[1] and cubemap[(i+1)%Nx,j,k]==markers[1] and cubemap[i,(j-1),k]==markers[1] and cubemap[i,(j+1)%Ny,k]==markers[1]: cubemap[i,j,k] = markers[2]
 					elif cubemap[i,(j-1),k]==markers[1] and cubemap[i,(j+1)%Ny,k]==markers[1] and cubemap[i,j,(k-1)]==markers[1] and cubemap[i,j,(k+1)%Nz]==markers[1]: cubemap[i,j,k] = markers[2]
@@ -35,8 +35,8 @@ def CubeMap(arr, multi_marker=True):
 	
 	## Cubes
 	for i in prange(Nx):
-		for j in xrange(Ny):
-			for k in xrange(Nz):
+		for j in range(Ny):
+			for k in range(Nz):
 				if cubemap[i,j,k] == 0:
 					if cubemap[(i-1),j,k]==markers[2] and cubemap[(i+1)%Nx,j,k]==markers[2]: 
 						if cubemap[i,(j-1),k]==markers[2] and cubemap[i,(j+1)%Ny,k]==markers[2]: 
@@ -50,8 +50,8 @@ def EulerCharacteristic_seq(A):
 	chi = 0;
 	nx,ny,nz = A.shape
 	for x in prange(nx):
-		for y in xrange(ny):
-			for z in xrange(nz):
+		for y in range(ny):
+			for z in range(nz):
 				if(A[x,y,z] == 1):
 					if (x+y+z)%2 == 0: chi += 1
 					else: chi -= 1
