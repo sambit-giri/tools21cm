0894855fcb84a365e378dce940038b9172d978a9
Garrelt Mellema <garrelt@astro.su.se>
2020-09-10 13:05:52 +0200

    Corrected a mistake in _dt_full. In response to Issue #8 by Nicolas Gillet.
     Changes to be committed:
    	modified:   temperature.py

--- t2c/temperature.py
+++ t2c/temperature.py
@@ -209,7 +209,7 @@ def _dt_full(rho, xi, Ts, z, correct):
                 # assuming that the ionized part of the cell is fully ionized
                 # (so a fraction xi of the cell's volume is ionized) and that
                 # the temperature of the ionized part is T_HII
-                Ts_new = Ts-xi*T_HII/(1.-xi)
+                Ts_new = (Ts-xi*T_HII)/(1.-xi)
                 Ts_new[Ts_new < T_min] = T_min
                 if np.any(Ts_new < 0): print("WARNING: negative temperatures")
         else:
