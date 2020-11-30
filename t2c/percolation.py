935ddb2384fa080a97b1dd4cd08484a140952360
sambit-giri <sambitgiri@outlook.com>
2020-11-30 15:23:12 +0100

    percolation theory

--- t2c/percolation.py
+++ t2c/percolation.py
@@ -28,6 +28,7 @@ def find_percolation_cluster(data, xth=0.5, connectivity=1):
 	out_map, size_list = bubble_stats.fof(data, xth=xth, connectivity=connectivity)
 	out_uniq  = np.unique(out_map, return_counts=1)
 	if out_uniq[0].size<2:
+		if out_uniq[0][0]==1: return out_map.size
 		print('There is no percolation cluster in the data.') 
 		return 0
 	out_label = np.zeros_like(out_map)
