2aece84855985a0395a1b65a1b1bf2bac7ee6b4e
Michele Bianco <42578987+micbia@users.noreply.github.com>
2021-02-22 19:40:56 +0100

    Add files via upload

--- t2c/density_file.py
+++ t2c/density_file.py
@@ -82,3 +82,63 @@ class DensityFile:
 		print_msg( '...done')
 
 
+class ClumpingFile:
+    '''
+    A CubeP3M clumping factor file.
+
+    Use the read_from_file method to load a clumping factor file, or 
+    pass the filename to the constructor.
+
+    Some useful attributes of this class are:
+
+    * raw_clumping (numpy array): the clumping factor in simulation units
+    * z (float): the redshift of the file (-1 if it couldn't be determined from the file name)
+
+    '''
+
+    def __init__(self, filename=None):
+        '''
+        Initialize the file. If filename is given, read data. Otherwise,
+        do nothing.
+
+        Parameters:
+            * filename = None (string): the file to read from.
+        Returns:
+            Nothing
+        '''
+        if filename:
+            self.read_from_file(filename)
+
+    def read_from_file(self, filename):
+        '''
+        Read data from file.
+
+        Parameters:
+            * filename (string): the file to read from.
+        Returns:
+            Nothing
+        '''
+
+        print('Reading clumping factor file: %s ...' %
+              (filename.rpartition("/")[-1]))
+        self.filename = filename
+        # Read raw data from clumping factor file
+        f = open(filename, 'rb')
+        temp_mesh = np.fromfile(f, count=3, dtype='int32')
+        self.mesh_x, self.mesh_y, self.mesh_z = temp_mesh
+        self.raw_clumping = np.fromfile(f, dtype='float32')
+        self.raw_clumping = self.raw_clumping.reshape((self.mesh_x, self.mesh_y, self.mesh_z), order='F')
+
+        f.close()
+        print('Average raw clumping:\t\t%.3e' % np.mean(self.raw_clumping))
+        print('Min and Max clumping:\t%.3e  %.3e' % (np.min(self.raw_clumping), np.max(self.raw_clumping)))
+
+        # Store the redshift from the filename
+        try:
+            import os.path
+            name = os.path.split(filename)[1]
+            self.z = float(name.split('c_')[0])
+        except:
+            print('Could not determine redshift from file name')
+            self.z = -1
+        print('...done')
