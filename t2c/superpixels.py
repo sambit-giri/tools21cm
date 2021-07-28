6b6434a46cf1782d32ba8e67e215e6aaa66f5de7
Sambit Kumar Giri <sambitgiri@outlook.com>
2021-07-28 11:44:54 +0200

    Update superpixels.py

--- t2c/superpixels.py
+++ t2c/superpixels.py
@@ -15,7 +15,8 @@ def slic_cube(cube, n_segments=5000, compactness=0.1, max_iter=20, sigma=0, min_
 	else:
 		multichannel = False
 	if verbose: print('Estimating superpixel labels using SLIC...')
-	labels = slic(cube, n_segments=n_segments, compactness=compactness, max_iter=max_iter, sigma=sigma, max_size_factor=max_size_factor, slic_zero=True, multichannel=multichannel, start_label=0)
+	try: labels = slic(cube, n_segments=n_segments, compactness=compactness, max_iter=max_iter, sigma=sigma, max_size_factor=max_size_factor, slic_zero=True, multichannel=multichannel, start_label=0)
+	except: labels = slic(cube, n_segments=n_segments, compactness=compactness, max_iter=max_iter, sigma=sigma, max_size_factor=max_size_factor, slic_zero=True, multichannel=multichannel)
 	if verbose: print("The output contains the labels with %d segments"%(labels.max()+1))
 	return labels
 
