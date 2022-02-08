c46866ef6479a3e740279cba546193e9de96c2ba
sambit-giri <sambitgiri@outlook.com>
2022-02-08 15:22:12 +0100

    changes to fix problems to work in develop mode

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ Created on 12 April 2017
 Setup script
 '''
 
-#from setuptools import setup, find_packages
-from distutils.core import setup
+from setuptools import setup, find_packages
+#from distutils.core import setup
 
 
 setup(name='tools21cm',
       version='2.0.1',
       author='Sambit Giri',
       author_email='sambit.giri@astro.su.se',
-      package_dir = {'tools21cm' : 't2c'},
-      packages=['tools21cm'],
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      # package_dir = {'tools21cm' : 'src'},
+      # packages=['tools21cm'],
       # package_data={'share':['*'],},
       package_data={'tools21cm': ['input_data/*']},
       install_requires=['numpy', 'scipy', 'matplotlib', 'numba',
