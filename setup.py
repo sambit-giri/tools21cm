d2886310ab9e9ccf2a8b7360670c7912c2dcc224
sambit-giri <sambitgiri@outlook.com>
2022-11-04 12:58:21 +0100

    documenting power_spect_fast

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='tools21cm',
-      version='2.0.1',
+      version='2.0.2',
       author='Sambit Giri',
       author_email='sambit.giri@astro.su.se',
       packages=find_packages("src"),
