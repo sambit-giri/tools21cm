0a4501d04905b50a6579d89698745bf3e7685324
sambit-giri <sambit.giri@gmail.com>
2022-11-10 16:24:00 +0100

    plotting the cylindrical PS.

--- src/tools21cm/power_spect_fast.py
+++ src/tools21cm/power_spect_fast.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 #from numba import jit, prange
 #from astropy.stats import histogram 
 from tqdm import tqdm
@@ -275,21 +276,77 @@ def power_spect_mu(input_array, kbins=10, box_dims=244/.7, return_modes=False, m
 	if return_modes: return ps, ks, mu, n_modes
 	return ps, ks, mu
 
+def plot_2d_power(ps, xlabel='$k_\perp$', ylabel='$k_\parallel$', ps_label='$P(k_\perp,k_\parallel)$',
+				  fig=None, plotting_scale={'x': 'log', 'y': 'log', 'z': 'log'}, 
+				  draw_wedge={'z': 9.0, 'fov_deg': 90.0, 'ls':'--', 'color': 'k'}, **kwargs):
+	'''
+	Plotting the 2D or cylindrical power spectrum.
+	
+	Parameters: 
+		ps (tuple or dict): The data in the form of tuple (Pk, kper_bins, kpar_bins) or
+							dictionary {'Pk': Pk, 'kper': kper_bins, 'kpar': kpar_bins}
+		xlabel = '$k_\perp$' : Label to use for the x-axis. 
+		ylabel = '$k_\parallel$' : Label to use for the y-axis. 
+		fig = None : Provide the matplotlib figure for plotting.
+		plotting_scale = {'x': 'log', 'y': 'log', 'z': 'log'} : Provide the plotting scales.
+		draw_wedge={'z': 9.0, 'fov_deg': 90.0, 'ls':'--', 'color': 'k'} : If not None, then the wedge is drawn.
 
-def plot_2d_power(ps, xticks, yticks, xlabel, ylabel):
+	Returns: 
+		The matplotlib figure where the power spectrum is plotted.
+	'''
 	import matplotlib.pyplot as plt
-	xticks, yticks = np.round(xticks, decimals=2), np.round(yticks, decimals=2)
-	plt.imshow(ps, origin='lower')
-	locs, labels = plt.yticks()
-	new_labels = yticks[locs.astype(int)[1:-1]]
-	plt.yticks(locs[1:-1], new_labels)
-	plt.ylabel(ylabel)
-	locs, labels = plt.xticks()
-	new_labels = xticks[locs.astype(int)[1:-1]]
-	plt.xticks(locs[1:-1], new_labels)
-	plt.xlabel(xlabel)
-	plt.colorbar()
+	import matplotlib.colors as colors
+
+	try: pp, kper, kpar = (ps[ke] for ke in ['Pk','kper','kpar'])
+	except: pp, kper, kpar = ps
+	fp = interpolate.interp2d(kper, kpar, pp.T, kind='linear')
+
+	if fig is None: fig, ax = plt.subplots(1,1,figsize=(7,5))
+	else: ax = fig.axes[0] 
+	X, Y = kper[:-1]/2+kper[1:]/2, kpar[:-1]/2+kpar[1:]/2
+	C = fp(X,Y)
+	norm = colors.LogNorm(vmin=C[np.isfinite(C)].min(), vmax=C[np.isfinite(C)].max()) if plotting_scale['z']=='log' else None 
+	pcm = ax.pcolormesh(X, Y, C, norm=norm, **kwargs)
+	if draw_wedge is not None:
+		f_kpar = horizon_wedge_equation(draw_wedge['z'], fov_deg=draw_wedge['fov_deg'])
+		ax.plot(X, f_kpar(X), ls=draw_wedge['ls'], color=draw_wedge['color'])
+		ax.axis([X.min(),X.max(),Y.min(),Y.max()])
+	fig.colorbar(pcm, label=ps_label)
+	ax.set_xlabel(xlabel)
+	ax.set_ylabel(ylabel)
+	ax.set_xscale(plotting_scale['x'])
+	ax.set_yscale(plotting_scale['y'])
 	plt.show()
+	return fig
+
+def horizon_wedge_equation(z, fov_deg=90.0):
+	'''
+	Equation 14 in arXiv:2201.10798
+
+	Parameters: 
+		z (float) : Redshift.
+		fov_deg = 90 : The field of view (FoV) of the horizon wedge equation in degrees.	
+	Returns: 
+		A lambda function k_parallel(k_perpendicular).
+	'''
+	f_kpar = lambda kper: kper*np.sin(fov_deg*np.pi/180)/(1+z)*\
+							t2c.hubble_parameter(z)/t2c.c*t2c.z_to_cdist(z)
+	return f_kpar
+
+# def plot_2d_power(ps, xticks, yticks, xlabel, ylabel):
+# 	import matplotlib.pyplot as plt
+# 	xticks, yticks = np.round(xticks, decimals=2), np.round(yticks, decimals=2)
+# 	plt.imshow(ps, origin='lower')
+# 	locs, labels = plt.yticks()
+# 	new_labels = yticks[locs.astype(int)[1:-1]]
+# 	plt.yticks(locs[1:-1], new_labels)
+# 	plt.ylabel(ylabel)
+# 	locs, labels = plt.xticks()
+# 	new_labels = xticks[locs.astype(int)[1:-1]]
+# 	plt.xticks(locs[1:-1], new_labels)
+# 	plt.xlabel(xlabel)
+# 	plt.colorbar()
+# 	plt.show()
 
 
 
