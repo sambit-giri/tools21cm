a7d2eaddfc20240a6ecf8d7baad2b1be42c45c4a
Michele Bianco <mbianco@protonmail.com>
2023-06-22 17:01:04 +0200

    corrected xfrac_file.py, pass more variable (for pyC2Ray), corrected the irate too

--- src/tools21cm/irate_file.py
+++ src/tools21cm/irate_file.py
@@ -14,7 +14,7 @@ class IonRateFile:
         z (float): the redshift of the file (-1 if it couldn't be determined from the file name)
     
     '''
-    def __init__(self, filename = None, old_format=False):
+    def __init__(self, filename = None, old_format=False, binary_format=False):
         '''
         Initialize the file. If filename is given, read data. Otherwise,
         do nothing.
@@ -27,9 +27,9 @@ class IonRateFile:
           Nothing
         '''
         if filename:
-            self.read_from_file(filename, old_format)
+            self.read_from_file(filename, old_format, binary_format)
 
-    def read_from_file(self, filename, old_format=False):
+    def read_from_file(self, filename, old_format=False, binary_format=False):
         '''
         Read data from file.
             
@@ -44,8 +44,12 @@ class IonRateFile:
         self.filename = filename
 
         f = open(filename, 'rb')
-        temp_mesh = np.fromfile(f, count=6, dtype='int32')
-        self.mesh_x, self.mesh_y, self.mesh_z = temp_mesh[1:4]
+        if(binary_format):
+            temp_mesh = np.fromfile(f, count=3, dtype='int32')
+            self.mesh_x, self.mesh_y, self.mesh_z = temp_mesh 
+        else:
+            temp_mesh = np.fromfile(f, count=6, dtype='int32')
+            self.mesh_x, self.mesh_y, self.mesh_z = temp_mesh[1:4]
 
         if old_format:
                 self.irate = np.fromfile(f, dtype='float32')
