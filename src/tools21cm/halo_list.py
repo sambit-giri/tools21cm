73acbd7fc157d50eed03d2ad565d26847141e104
sambit-giri <sambit.giri@gmail.com>
2023-11-30 17:38:08 +0100

    full halo list reader

--- src/tools21cm/halo_list.py
+++ src/tools21cm/halo_list.py
@@ -1,5 +1,6 @@
 import numpy as np
 from glob import glob
+from time import time
 from .helper_functions import print_msg
 from . import const
 from . import conv
@@ -128,8 +129,127 @@ class HaloRockstar:
 		return data
 
 
+class HaloCubeP3MFull:
+        '''
+        Adapted from HaloList
+        A class that holds information about a large number of halos, as read from a 
+        cube3pm halo list file - the full catalogue.
+        Contains methods to select halos based on different criteria. This file is very slow
+        if you need to read a large number of halos.
+        '''
+        def __init__(self, filename=None, min_select_mass=0.0, max_select_mass=None, max_select_number=-1, startline = 0):
+                '''
+                Initialize the object. If filename is given, read the file. Otherwise,
+                do nothing.
+                
+                Parameters:
+                        * filename = None (string): The file to read from
+                        * min_select_mass = 0.0 (float): The lower threshold mass in solar masses.
+                                Only halos above this mass will be read.
+                        * max_select_mass = None (float): The upper threshold mass in solar masses.
+                                Only halos below this mass will be read. If None, there is no limit.
+                        * max_select_number = -1 (int): The max number of halos to read. If -1, there
+                                is no limit.
+                        * startline = 0 (int): The line in the file where reading will start.
+                Returns:
+                        Nothing
+                '''
+                self.halos = []
 
-class HaloCube3PM:
+                if filename:
+                        self.read_from_file(filename, min_select_mass, max_select_mass, max_select_number, 
+                                        startline)
+
+        def read_from_file(self,filename, min_select_mass = 0.0, max_select_mass = None, max_select_number=-1, 
+                        startline=0):
+                '''
+                Read a halo list.
+                
+                Parameters:
+                        * filename (string): The file to read from
+                        * min_select_mass = 0.0 (float): The lower threshold mass in solar masses.
+                                Only halos above this mass will be read.
+                        * max_select_mass = None (float): The upper threshold mass in solar masses.
+                                Only halos below this mass will be read. If None, there is no limit.
+                        * max_select_number = -1 (int): The max number of halos to read. If -1, there
+                                is no limit.
+                        * startline = 0 (int): The line in the file where reading will start.
+                Returns:
+                        True if all the halos were read. False otherwise.
+                '''
+
+                self.halos = []
+                # self.halo_pos = []
+
+                print_msg('Reading halo file %s...' % filename)
+                self.filename = filename
+                import fileinput
+                #Store the redshift from the filename
+                import os.path
+                name = os.path.split(filename)[1]
+                self.z = float(name.split('halo')[0])
+
+                # Read the file line by line, since it's large
+                linenumber = 1
+                skipped_lines = 0
+                tstart = time()
+				
+                min_select_grid_mass = min_select_mass/(conv.M_grid*const.solar_masses_per_gram)
+                if max_select_mass:
+                        print_msg('Max_select_mass: %g' % max_select_mass)
+                        max_select_grid_mass = max_select_mass/(conv.M_grid*const.solar_masses_per_gram)
+
+                for line in fileinput.input(filename):
+                        if linenumber < startline: #If you want to read from a particular line
+                                linenumber += 1
+                                continue
+                        if max_select_number >= 0 and len(self.halos) >= max_select_number:
+                                fileinput.close()
+                                return False
+                        if linenumber % 100000 == 0:
+                                print_msg(f'{linenumber} lines read in {(time()-tstart):.2f} s' )
+                        linenumber += 1
+                        # print('LINE:', line)
+                        vals = line.split()
+                        if len(vals)==1:
+                                skipped_lines += 1
+                                # print('skipping this line')
+                                continue
+                        # print('VALS:', vals)
+                        grid_mass = float(vals[-3])
+
+                        # Create a halo and add it to the list
+                        if grid_mass > min_select_grid_mass and (max_select_mass == None or grid_mass < max_select_grid_mass):
+                                halo = Halo()
+                                # The following lines used the map() function to convert
+                                # parts of the vals list into floats before putting them
+                                # into an array. In Python 3 map() returns an iterable,
+                                # not a list, so changed this to a list operation.
+                                # GM/200601
+
+                                halo.pos    = np.array([float(i) for i in vals[:3]])
+                                halo.pos_cm = np.array([float(i) for i in vals[3:6]])
+                                halo.vel = np.array([float(i) for i in vals[6:9]])
+                                halo.l   = np.array([float(i) for i in vals[9:12]])
+                                halo.vel_disp = np.array([float(i) for i in vals[12:15]])
+                                halo.m  = float(vals[16])
+                                halo.mp = float(vals[17])
+                                halo_mass_1 = float(vals[18])
+                                var_x = np.array([float(i) for i in vals[19:22]])
+                                halo.solar_masses = grid_mass*conv.M_grid*const.solar_masses_per_gram
+                                self.halos.append(halo)
+
+                        # if linenumber==50:
+                        #         break
+
+                fileinput.close()
+                
+                print(f'Total number of lines read: {linenumber-1}')
+                print(f'Number of lines skipped   : {skipped_lines}')
+                print(f'Total time elasped        : {(time()-tstart):.2f} s')
+                return True
+
+class HaloCubeP3M:
 	'''
 	A CubeP3M Halo cataloge files have the following structure:
 
