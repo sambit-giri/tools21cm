91a5b6c3bf62b72f1ac6964a9e6ec365b9398337
sambit-giri <sambit.giri@gmail.com>
2023-12-08 17:31:02 +0100

    updated version that contains class to read the full CubeP3M halo catalogue.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='tools21cm',
-      version='2.1.2',
+      version='2.1.3',
       author='Sambit Giri',
       author_email='sambit.giri@astro.su.se',
       packages=find_packages("src"),
