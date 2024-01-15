37a74131043810493ae35a4d1d480019dfecfa48
sambit-giri <sambit.giri@gmail.com>
2024-01-15 11:19:36 +0100

    full particles info reader

--- src/tools21cm/nbody_pkdgrav.py
+++ src/tools21cm/nbody_pkdgrav.py
@@ -48,6 +48,33 @@ class ReaderPkdgrav3:
         """
         return 8677.2079486362706 / dMpcUnit
     
+    def read_particles(self, filename):
+        tipsy = open(filename,'rb')
+        header_type = np.dtype([('time', '>f8'),('N', '>i4'), ('Dims', '>i4'), ('Ngas', '>i4'), ('Ndark', '>i4'), ('Nstar', '>i4'), ('pad', '>i4')])
+        gas_type  = np.dtype([('mass','>f4'), ('x', '>f4'),('y', '>f4'),('z', '>f4'), ('vx', '>f4'),('vy', '>f4'),('vz', '>f4'),
+                            ('rho','>f4'), ('temp','>f4'), ('hsmooth','>f4'), ('metals','>f4'), ('phi','>f4')])
+        dark_type = np.dtype([('mass','>f4'), ('x', '>f4'),('y', '>f4'),('z', '>f4'), ('vx', '>f4'),('vy', '>f4'),('vz', '>f4'),
+                            ('eps','>f4'), ('phi','>f4')])
+        star_type  = np.dtype([('mass','>f4'), ('x', '>f4'),('y', '>f4'),('z', '>f4'), ('vx', '>f4'),('vy', '>f4'),('vz', '>f4'),
+                            ('metals','>f4'), ('tform','>f4'), ('eps','>f4'), ('phi','>f4')])
+        
+        header = np.fromfile(tipsy,dtype=header_type,count=1)
+        header = dict(zip(header_type.names,header[0]))
+        gas  = np.fromfile(tipsy,dtype=gas_type,count=header['Ngas'])
+        gas  = pd.DataFrame(gas,columns=gas.dtype.names)
+        dark = np.fromfile(tipsy,dtype=dark_type,count=header['Ndark'])
+        dark = pd.DataFrame(dark,columns=dark.dtype.names)
+        star = np.fromfile(tipsy,dtype=star_type,count=header['Nstar'])
+        star = pd.DataFrame(star,columns=star.dtype.names)
+        tipsy.close()
+        
+        data = {}
+        data['header'] = header
+        data['gas']  = gas
+        data['dark'] = dark
+        data['star'] = star
+        return data
+    
 class HaloCataloguePkdgrav3(ReaderPkdgrav3):
 
     def __init__(self, box_len, nGrid, 
